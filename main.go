c8699aefd2949351f59483f1a521c1dd9e6058df
xiote <xiote@nate.com>
2020-01-28 19:47:22 +0900

    Update main

--- main.go
+++ main.go
@@ -1,1 +1,22 @@
 package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	scanner := bufio.NewScanner(os.Stdin)
+	fmt.Printf("What is your name? ")
+	for scanner.Scan() {
+		var name string = scanner.Text()
+		var message = Hello(name)
+		fmt.Println(message)
+		fmt.Println()
+		fmt.Printf("What is your name? ")
+	}
+	if err := scanner.Err(); err != nil {
+		fmt.Fprintln(os.Stderr, "reading standard input:", err)
+	}
+}
